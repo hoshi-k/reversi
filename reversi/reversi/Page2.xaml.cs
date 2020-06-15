d70e08b90f164c1fe4ecc650fcee779ee15fe6ac
kaiRyusei <64994665+kaiRyusei@users.noreply.github.com>
2020-06-15 15:44:32 +0900

    ・「おけるかどうかのチェック」を追加

--- reversi/reversi/Page2.xaml.cs
+++ reversi/reversi/Page2.xaml.cs
@@ -25,26 +25,19 @@ namespace オセロ
         ImageSource IMG_WHITE;
         ImageSource IMG_BLACK;
         bool firstPlayerTarn = true;
-        //int[] row0 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-        //int[] row1 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-        //int[] row2 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-        //int[] row3 = { 0, 0, 0, 1, 2, 0, 0, 0 };
-        //int[] row4 = { 0, 0, 0, 2, 1, 0, 0, 0 };
-        //int[] row5 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-        //int[] row6 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-        //int[] row7 = { 0, 0, 0, 0, 0, 0, 0, 0 };
-
-        int[] row0 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        int[] row1 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-        int[] row2 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-        int[] row3 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-        int[] row4 = { 0, 1, 1, 1, 3, 2, 1, 1, 1, 0 };
-        int[] row5 = { 0, 1, 1, 1, 2, 3, 1, 1, 1, 0 };
-        int[] row6 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-        int[] row7 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-        int[] row8 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-        int[] row9 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
+        List<int[]> rowList = new List<int[]>()
+        {
+        new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 2, 3, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 3, 2, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        new int[] { 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+        new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    };
         public Page2()
         {
             InitializeComponent();
@@ -52,7 +45,16 @@ namespace オセロ
             IMG_BLACK = btn45.Source;
             IMG_GREEN = btn11.Source;
             ReflectOnBoard();
+            //button1.BackgroundColor = Color.Red;
         }
+
+        //Image myImage3 = new Image();
+        //BitmapImage bi3 = new BitmapImage();
+        //bi3.BeginInit();
+        //bi3.UriSource = new Uri("smiley_stackpanel.PNG", UriKind.Relative);
+        //bi3.EndInit();
+        //myImage3.Stretch = Stretch.Fill;
+        //myImage3.Source = bi3;
 
         private void ChangeTarn()
         {
@@ -78,53 +80,263 @@ namespace オセロ
             }
         }
 
-        //置こうとしたマスがおける場所なのかチェックする関数
-        private void CheckTurnUpsideDown(int btnName2)
+        private int GetPutStoneColoer()
         {
-            int tens = (btnName2 / 10) % 10; // (n % 100) / 10
-            int ones = btnName2 % 10;
-            int[] row = GetRow(tens);
-            if (row[ones] == 0)
+            if (firstPlayerTarn)
             {
-                //TODO:配列の範囲外になってしまいチェックできない　配列の端一列ずつ追加する？
+                return 3;//黒
+            }
+            else
+            {
+                return 2;//白
             }
         }
 
-        private int[] GetRow(int row)
+        //置こうとしたマスがおける場所なのかチェックする関数
+        private void CheckTurnUpsideDown(int btnName2)
         {
-            switch (row)
+            int tens = (btnName2 / 10) % 10;//btnName2の１０の位の数
+            int ones = btnName2 % 10;//btnName2の１の位の数
+            if (rowList[tens][ones] == 1)//置きたい場所が緑であるとき
             {
-                case 0:
-                    return row0;
-                case 1:
-                    return row1;
-                case 2:
-                    return row2;
-                case 3:
-                    return row3;
-                case 4:
-                    return row4;
-                case 5:
-                    return row5;
-                case 6:
-                    return row6;
-                case 7:
-                    return row7;
-                case 8:
-                    return row8;
-                case 9:
-                    return row9;
-                default:
-                    return row0;
+                if (RightCheck(tens, ones) || 
+                    LeftCheck(tens, ones) || 
+                    UpCheck(tens, ones) || 
+                    DownCheck(tens, ones)||
+                    RightCheck(tens, ones) ||
+                    LeftCheck(tens, ones) ||
+                    UpCheck(tens, ones) ||
+                    DownCheck(tens, ones)) //ひっくり返せるか判定する処理
+                {
+                    //ひっくり返す処理
+                }
+            }
+            else
+            {
+                //表示：おけない場所です
             }
         }
+
+
+        #region ひっくり返せるか判定する処理
+
+        private bool RightCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            if (rowList[tens][ones + 1] != color)//一つ右が相手の石の時
+            {
+                for (int i = ones + 2; rowList[tens][i] != 0; ones++)//2つ右以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[tens][i] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[tens][i] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool LeftCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            if (rowList[tens][ones - 1] != color || rowList[tens][ones - 1] != 0)//一つ左が相手の石の時
+            {
+                for (int i = ones - 2; rowList[tens][i] != 0; ones--)//2つ右以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[tens][i] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[tens][i] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool UpCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            if (rowList[tens+1][ones] != color || rowList[tens+1][ones] != 0)//一つ上が相手の石の時
+            {
+                for (int j = tens + 2; rowList[tens][ones] != 0; tens++)//2つ上以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[j][ones] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[j][ones] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool DownCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            if (rowList[tens - 1][ones] != color || rowList[tens - 1][ones] != 0)//一つ上が相手の石の時
+            {
+                for (int j = tens - 2; rowList[tens][ones] != 0; tens--)//2つ上以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[j][ones] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[j][ones] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool RightUpCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            int k = 2;
+            if (rowList[tens + 1][ones + 1] != color)//一つ右上が相手の石の時
+            {
+                while (rowList[tens + k][ones + k] != 0)//2つ右以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[tens + k][ones + k] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[tens + k][ones + k] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool LeftUpCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            int k = 2;
+            if (rowList[tens + 1][ones - 1] != color)//一つ左上が相手の石の時
+            {
+                while (rowList[tens + k][ones + k] != 0)//2つ左上以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[tens + k][ones - k] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[tens + k][ones - k] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool RightDownCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            int k = 2;
+            if (rowList[tens - 1][ones + 1] != color)//一つ右下が相手の石の時
+            {
+                while (rowList[tens - k][ones + k] != 0)//2つ右下以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[tens - k][ones + k] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[tens - k][ones + k] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool LeftDownCheck(int tens, int ones)
+        {
+            int color = GetPutStoneColoer();
+            int k = 2;
+            if (rowList[tens - 1][ones - 1] != color)//一つ左下が相手の石の時
+            {
+                while (rowList[tens - k][ones - k] != 0)//2つ左下以降のチェック　番外になったら抜ける
+                {
+                    if (rowList[tens - k][ones - k] == color)//自分の色のとき
+                    {
+                        return true;
+                    }
+                    else if (rowList[tens - k][ones - k] == 1)//緑のとき抜ける
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                }
+            }
+            return false;
+        }
+
+        #endregion
+
+        //private int[] GetRow(int row)
+        //{
+        //    switch (row)
+        //    {
+        //        case 0:
+        //            return row0;
+        //        case 1:
+        //            return row1;
+        //        case 2:
+        //            return row2;
+        //        case 3:
+        //            return row3;
+        //        case 4:
+        //            return row4;
+        //        case 5:
+        //            return row5;
+        //        case 6:
+        //            return row6;
+        //        case 7:
+        //            return row7;
+        //        case 8:
+        //            return row8;
+        //        case 9:
+        //            return row9;
+        //        default:
+        //            return row0;
+        //    }
+        //}
 
         //最後に盤面を画面に反映する関数
         private void ReflectOnBoard()
         {
             for (int row = 0; row < 8; row++)
             {
-                int[] rowwww = GetRow(row);
+                int[] rowwww = rowList[row];
                 for (int line = 0; line < 8; line++)
                 {
                     string rowstr = row.ToString();
